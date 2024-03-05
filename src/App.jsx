7eefe76de2370298907e8caf311ba25263882816
shiv_bilgundi <shivbilgundiyt@gmail.com>
2024-03-06 00:08:51 +0530

    add -breadCrumbs ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { ChevronsRight } from 'lucide-react';
 import Navbar from './components/Navbar';
+import { useParams } from 'react-router-dom';
 
 const App = () => {
+  const params = useParams();
+
   return (
     <>
       <Navbar />
+
+      {/* ----Breadcrumbs---- */}
+      <div className='container mx-auto m-4 p-1'>
+        <span className='flex items-center'>
+          <p className='text-sm text-gray'>Cryptocurrencies </p>
+          <ChevronsRight className='text-[10px] text-gray' />
+          <p className='text-black text-sm font-medium capitalize cursor-pointer'>
+            {' '}
+            {params?.coins}
+          </p>
+        </span>
+      </div>
     </>
   );
 };
