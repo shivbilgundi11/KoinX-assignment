f83ee249709674c4717b898623d8ddf9a8cd7219
shiv_bilgundi <shivbilgundiyt@gmail.com>
2024-03-07 15:21:05 +0530

    api -fetched dynamic data for coin logo, name, symbol

---
+++ src/components/(ui-components)/Loader.jsx
@@ -0,0 +1,7 @@
+import { Loader } from 'lucide-react';
+
+const Loading = () => {
+  return <Loader className=' animate-spin transition text-center mx-auto' />;
+};
+
+export default Loading;
---
+++ src/utils/utils.js
@@ -0,0 +1,3 @@
+const coinBasicsURL = `https://api.coingecko.com/api/v3`;
+
+export { coinBasicsURL };
